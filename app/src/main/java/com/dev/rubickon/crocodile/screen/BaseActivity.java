9c730d1635e7f0d3e40a4a895469d9e2210b44ca
EviLPinGuiN <shaihrazievnail@gmail.com>
2017-07-12 11:54:00 +0300

    migrate to 25 api

--- app/src/main/java/com/dev/rubickon/crocodile/screen/BaseActivity.java
+++ app/src/main/java/com/dev/rubickon/crocodile/screen/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.dev.rubickon.crocodile.screen;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,6 +8,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.widget.TextView;
 
 
 import com.dev.rubickon.crocodile.R;
@@ -73,6 +82,25 @@ public class BaseActivity extends AppCompatActivity {
             }
             return true;
         });
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/round_script.ttf");
+        View header = mNavigationView.getHeaderView(0);
+        final TextView tv_title = (TextView) header.findViewById(R.id.tv_main_title);
+        tv_title.setTypeface(font);
+        Menu m = mNavigationView .getMenu();
+        for (int i=0;i<m.size();i++) {
+            MenuItem mi = m.getItem(i);
+            SubMenu subMenu = mi.getSubMenu();
+            if (subMenu!=null && subMenu.size() >0 ) {
+                for (int j=0; j <subMenu.size();j++) {
+                    MenuItem subMenuItem = subMenu.getItem(j);
+                    SpannableString s = new SpannableString(subMenuItem.getTitle());
+                    s.setSpan(new TypefaceSpan("fonts/round_script.ttf"), 0, s.length(),
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    subMenuItem.setTitle(s);
+                }
+            }
+        }
+
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar,
                 R.string.drawer_open, R.string.drawer_close);
         mDrawer.addDrawerListener(actionBarDrawerToggle);
